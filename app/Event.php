e6dc173071c46373afdfd705830f49e09382a03f
Spencer Thang <spencerthang@gmail.com>
2016-02-21 06:10:05 +0000

    Add date mutators to event/event attendee.

--- app/Event.php
+++ app/Event.php
@@ -9,6 +9,13 @@ class Event extends Model
     // Table used to store information for this model
     protected $table = 'events';
 
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
     /*
      * Get the attendees for this event.
      */
--- app/EventAttendee.php
+++ app/EventAttendee.php
@@ -10,6 +10,13 @@ class EventAttendee extends Model
     protected $table = 'event_attendees';
 
     /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['time_admitted', 'created_at', 'updated_at', 'deleted_at'];
+
+    /**
      * Get the event this attendee belongs to.
      */
     public function event()
