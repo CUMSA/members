6efa96439153b68d022ffaca864cbfa78ded7547
Spencer Thang <spencerthang@gmail.com>
2016-02-21 03:32:38 +0000

    Database: create events table.

---
+++ database/migrations/2016_02_21_030340_create_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->string('name');
+            $table->dateTime('datetime_start');
+            $table->dateTime('datetime_end');
+            $table->text('description');
+            $table->text('location');
+            $table->text('comments');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('events');
+    }
+}
