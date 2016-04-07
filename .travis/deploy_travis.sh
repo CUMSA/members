3ba1f02aac878ad1ed8009aedcba4f61693a1b86
Daniel Wong <wonglkd@gmail.com>
2016-04-07 01:38:29 +0100

    Set up beta staging area

--- .travis/deploy_travis.sh
+++ .travis/deploy_travis.sh
@@ -2,6 +2,12 @@ openssl aes-256-cbc -K $encrypted_7edbf22d25f4_key -iv $encrypted_7edbf22d25f4_i
 eval "$(ssh-agent -s)" #start the ssh agent
 chmod 600 .travis/cumsa_ci_rsa # this key should have push access
 ssh-add .travis/cumsa_ci_rsa
-git remote add deploy ssh://cumsaorg@cumsa.org/home/cumsaorg/members-repo
-git push deploy
-ssh cumsaorg@cumsa.org "cd /home/cumsaorg/members-repo && git reset --hard && bash .travis/deploy.sh"
+if [ "master" = "$TRAVIS_BRANCH" ] then
+    git remote add deploy ssh://cumsaorg@cumsa.org/home/cumsaorg/members-repo
+    git push deploy
+    ssh cumsaorg@cumsa.org "cd /home/cumsaorg/members-repo && git reset --hard && bash .travis/deploy.sh"
+else
+    git remote add deploy ssh://cumsaorg@cumsa.org/home/cumsaorg/members-beta-repo
+    git push deploy
+    ssh cumsaorg@cumsa.org "cd /home/cumsaorg/members-beta-repo/ && git reset --hard && bash .travis/deploy.sh"
+fi
