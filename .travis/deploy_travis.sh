2a253925e2ec06cb8e17059c812fea9be9864e69
Daniel Wong <wonglkd@gmail.com>
2016-04-07 01:50:44 +0100

    Bugfix: for the want of a semi-colon

--- .travis/deploy_travis.sh
+++ .travis/deploy_travis.sh
@@ -2,7 +2,7 @@ openssl aes-256-cbc -K $encrypted_7edbf22d25f4_key -iv $encrypted_7edbf22d25f4_i
 eval "$(ssh-agent -s)" #start the ssh agent
 chmod 600 .travis/cumsa_ci_rsa # this key should have push access
 ssh-add .travis/cumsa_ci_rsa
-if [ "master" = "$TRAVIS_BRANCH" ] then
+if [ "master" = "$TRAVIS_BRANCH" ]; then
     git remote add deploy ssh://cumsaorg@cumsa.org/home/cumsaorg/members-repo
     git push deploy
     ssh cumsaorg@cumsa.org "cd /home/cumsaorg/members-repo && git reset --hard && bash .travis/deploy.sh"
