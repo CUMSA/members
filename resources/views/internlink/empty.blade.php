619c6f7c0f245b010bf190773fe627a19f646b6b
tanhanliang <tanhanliang@outlook.com>
2016-09-27 02:49:36 +0800

    Remove view for no search results

--- resources/views/internlink/empty.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.panel')
-@section('panel-cols', '10')
-@section('panel-offset', '1')
-@section('panel-heading', 'InternLink')
-@section('panel-body')
-    <h2>Your search returned no results</h2>
-@endsection
